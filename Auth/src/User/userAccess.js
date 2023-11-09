37719012043973cfc8d40ef24d3ea1ac0e14b47d
Abhishek Kumar Singh <abhishek81121@gmail.com>
2023-11-09 14:57:19 +0530

    S33-14 fix: Now incorporates access token for multiple sessions(User)

--- Auth/src/User/userAccess.js
+++ Auth/src/User/userAccess.js
@@ -4,16 +4,24 @@ import { riderRepositiory } from "../../User.js";
 var userAccess = express.Router();
 import "dotenv/config";
 userAccess.post("/user/access", async (req, res) => {
+  function incudedInObj(Object, element) {
+    for (let i = 0; i < Object.length; i++) {
+      if (Object[i] == element) {
+        return true;
+      }
+    }
+    return false;
+  }
   const accessToken = await req.body.accessToken;
   const email = await req.body.email;
   try {
     const emailVer = await jwt.verify(accessToken, process.env.ACCESS_SECRET)
       .email;
+
     if (email == emailVer) {
       const accessTokenVer = await riderRepositiory.fetch(email);
-      if (
-        accessToken == (accessTokenVer.access ? accessTokenVer.access : null)
-      ) {
+      console.log(accessTokenVer.accessTokens);
+      if (incudedInObj(accessTokenVer.accessTokens, accessToken)) {
         res.send(JSON.stringify("Authorized"));
       } else {
         res.status(400).send("Probably a malicious user");
